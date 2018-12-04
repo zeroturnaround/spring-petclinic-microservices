d333f82bb67d102a6cc0d2179033c604d93e6bf9
Sten <sten@Stens-MacBook.local>
2018-12-04 08:39:21 +0200

    Fix endpoint to load Vet Details

--- spring-petclinic-api-gateway/src/main/resources/static/scripts/vet-details/vet-details.controller.js
+++ spring-petclinic-api-gateway/src/main/resources/static/scripts/vet-details/vet-details.controller.js
@@ -4,7 +4,7 @@ angular.module('vetDetails')
     .controller('VetDetailsController', ['$http', '$stateParams', function ($http, $stateParams) {
         var self = this;
 
-        $http.get('api/gateway/vets/' + $stateParams.vetId).then(function (resp) {
+        $http.get('api/vet/vets/' + $stateParams.vetId).then(function (resp) {
             self.vet = resp.data;
         });
     }]);
